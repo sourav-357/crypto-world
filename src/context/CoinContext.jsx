6e755bb8a40e2c83ebabfc6868ac398f973c902c
Sourav Kumar <xsouravkumar357@gmail.com>
2025-12-18 19:36:22 +0530

    Completed the half of Project - leftout is the dynamic and clickable part

---
+++ src/context/CoinContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useEffect, useState } from "react";
+
+export const CoinContext = createContext();
+
+export const CoinContextProvider = (props) => {
+  const [allCoin, setallCoin] = useState([]);
+  const [currency, setCurrency] = useState({
+    name: "USD",
+    symbol: "$",
+  });
+
+  const fetchAllCoin = () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-wEhRCmzLBCegVmaazEGhLokR",
+      },
+    };
+
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setallCoin(response))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchAllCoin();
+  }, [currency]);
+
+  const contextValue = {
+    allCoin,
+    currency,
+    setCurrency,
+  };
+
+  return (
+    <CoinContext.Provider value={contextValue}>
+      {props.children}
+    </CoinContext.Provider>
+  );
+};
+
+export default CoinContextProvider;
